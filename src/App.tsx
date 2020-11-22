860abdc0c4d96c5031fae77af2017dbf4a794c37
autsada <autsada.t@gmail.com>
2020-11-22 14:16:58 +0700

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+type CountState = { count: number }
+
+type CountAction =
+  | { type: 'PLUS'; payload: number }
+  | { type: 'MINUS'; payload: number }
+  | { type: 'MULTIPLY'; payload: number }
+  | { type: 'DIVIDE'; payload: number }
+
+const countReducer = (state: CountState, action: CountAction): CountState => {
+  switch (action.type) {
+    case 'PLUS':
+      return { count: state.count + action.payload }
+
+    case 'MINUS':
+      return { count: state.count - action.payload }
+
+    case 'MULTIPLY':
+      return { count: state.count * action.payload }
+
+    case 'DIVIDE':
+      return { count: state.count / action.payload }
+
+    default:
+      return state
+  }
 }
 
-export default App;
+const initialState: CountState = { count: 0 }
+
+function App() {
+  const [countState, countDispatch] = useReducer(countReducer, initialState)
+
+  return (
+    <div className='App'>
+      <h3>Count: {countState.count}</h3>
+
+      <div>
+        <button
+          style={{ cursor: 'pointer' }}
+          onClick={() => countDispatch({ type: 'PLUS', payload: 2 })}
+        >
+          PLUS 2
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ cursor: 'pointer' }}
+          onClick={() => countDispatch({ type: 'MINUS', payload: 2 })}
+        >
+          MINUS 2
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ cursor: 'pointer' }}
+          onClick={() => countDispatch({ type: 'MULTIPLY', payload: 2 })}
+        >
+          MULTIPLY 2
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ cursor: 'pointer' }}
+          onClick={() => countDispatch({ type: 'DIVIDE', payload: 2 })}
+        >
+          DIVIDE 2
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
